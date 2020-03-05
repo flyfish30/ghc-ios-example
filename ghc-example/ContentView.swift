8807ae8822996381c54e29b37ecab20c7ac584ec
Parker Liu <liuchangsheng30@gmail.com>
2020-03-05 10:24:58 +0800

    Initial Commit

---
+++ ghc-example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ghc-example
+//
+//  Created by Changsheng Liu on 2020/3/5.
+//  Copyright © 2020 Changsheng Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ghc-exampleTests/ghc_exampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ghc_exampleTests.swift
+//  ghc-exampleTests
+//
+//  Created by Changsheng Liu on 2020/3/5.
+//  Copyright © 2020 Changsheng Liu. All rights reserved.
+//
+
+import XCTest
+@testable import ghc_example
+
+class ghc_exampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
